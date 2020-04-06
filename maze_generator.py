52b36b0a5aad93cf913eaf7ae94d3a8e9c02b815
LastGenius-edu <sultanov@ucu.edu.ua>
2020-04-06 17:59:40 +0300

    shift from recursion to dynamic stack, takes a bit of RAM to generate a huge maze but it works!!!!

--- maze_generator.py
+++ maze_generator.py
@@ -6,7 +6,7 @@ from PIL import Image
 from time import sleep
 
 
-def make_maze(width=16, height=8):
+def breadth_first_generation(width=64, height=64):
     """
     Making a maze.
     Each cell starts out with all 4 walls.
@@ -26,52 +26,45 @@ def make_maze(width=16, height=8):
     matrix += [[255]*(2*width + 5)]*2
 
     def walk(x, y):
+        stack = []
         visited[y][x] = True
+        stack.append((x, y))
 
-        # translate the coordinates into real pixel cells
-        real_x, real_y = 2*x+1, 2*y+1
+        while len(stack) > 0:
+            x, y = stack.pop(-1)
+            # translate the coordinates into real pixel cells
+            real_x, real_y = 2*x+1, 2*y+1
 
-        # generate a random move to the neighbor cell
-        d = [((x-1, y), (real_x-1, real_y)),
-             ((x, y+1), (real_x, real_y+1)),
-             ((x+1, y), (real_x+1, real_y)),
-             ((x, y-1), (real_x, real_y-1))]
-        shuffle(d)
+            # generate a random move to the neighbor cell
+            d = [((x-1, y), (real_x-1, real_y)),
+                 ((x, y+1), (real_x, real_y+1)),
+                 ((x+1, y), (real_x+1, real_y)),
+                 ((x, y-1), (real_x, real_y-1))]
+            shuffle(d)
 
-        for choice in d:
-            xx, yy = choice[0]
-            real_xx, real_yy = choice[1]
+            for choice in d:
+                xx, yy = choice[0]
+                real_xx, real_yy = choice[1]
 
-            if visited[yy][xx]:
-                continue
+                if visited[yy][xx]:
+                    continue
 
-            matrix[real_yy][real_xx] = 0
+                matrix[real_yy][real_xx] = 0
+                stack.append((x, y))
+                stack.append((xx, yy))
+                visited[yy][xx] = True
 
-            # generate a live image from the array
-            npmatrix = np.array(matrix)
-            image = Image.fromarray(npmatrix.astype('uint8'))
-            image.save('image.jpg')
-
-            # print out a debug matrix
-            os.system('cls')
-            for row in matrix:
-                row_chars = ''
-                for col in row:
-                    if col:
-                        row_chars += "O"
-                    else:
-                        row_chars += " "
-                print(row_chars)
-
-            # sleep(0.2)
-            walk(xx, yy)
+        # generate a live image from the array
+        npmatrix = np.array(matrix)
+        image = Image.fromarray(npmatrix.astype('uint8'))
+        image.save(f'image.jpg')
 
     # walk(randint(0, width-1), randint(0, height-1))
     walk(1, 1)
 
 
 def main():
-    make_maze()
+    breadth_first_generation()
 
 
 if __name__ == '__main__':
